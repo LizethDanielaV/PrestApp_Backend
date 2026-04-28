972ae08a8151958b0c3ec3631c8652a93b1a3c3e
LizethDanielaV <lizethdanielavb@ufps.edu.co>
2026-04-28 18:36:15 -0500

    Aceptando cambio

--- prestamos/models.py
+++ prestamos/models.py
@@ -32,7 +32,7 @@ class Referencia(models.Model):
     nombre = models.CharField(max_length=100)
     celular = models.CharField(max_length=15)
     direccion = models.CharField(max_length=300)
-
+ 
     def __str__(self):
         return self.nombre
 
